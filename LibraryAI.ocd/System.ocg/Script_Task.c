6016bc18f8ae10edd8eb6f25a76243c06570c1d9
Mark <markh4b@hotmail.com>
2017-06-03 21:57:34 +0200

    Task: Additional definitions

--- LibraryAI.ocd/System.ocg/Script_Task.c
+++ LibraryAI.ocd/System.ocg/Script_Task.c
@@ -4,6 +4,8 @@
 
 static const Task = new Effect 
 {
+	/* -- Interface --*/
+
 	SetPriority = func (int priority)
 	{
 		this.task_data.priority = priority;
@@ -18,9 +20,31 @@ static const Task = new Effect
 	{
 		return this.task_data.transferrable;
 	},
+	
+	/* -- Callbacks -- */
+	
+	Construction = func()
+	{
+		this.Interval = 0; // never calls a timer
+		
+		if (this.Priority) // delegate the priority to the internal stuff, too
+		{
+			this->SetPriority(this.Priority);
+		}
+
+		return FX_OK;
+	},
+	
+	/* -- Internals -- */
 
 	task_data = {
 		priority = 0,
 		transferrable = false,
+	},
+	
+	/* -- Execution -- */
+	
+	Execute = func (proplist controller, object agent)
+	{
 	},
 };
