a00416c823b6681b7f98415ea3d1ffaddf5f70a4
Mark <markh4b@hotmail.com>
2017-06-05 09:40:30 +0200

    Test: Get item outside of inventory

--- LibraryAI.ocd/Tests.ocf/TaskAI.ocs/Script.c
+++ LibraryAI.ocd/Tests.ocf/TaskAI.ocs/Script.c
@@ -19,7 +19,18 @@ global func OnTaskSuccess(proplist task)
 	CurrentTest().TaskSuccess = true;
 }
 
+global func OnTestFinished()
+{
+	// Default cleanup
+	if (CurrentTest().Bot) CurrentTest().Bot->RemoveObject();
+	if (CurrentTest().Item) CurrentTest().Item->RemoveObject();
+	CurrentTest().AddedAI = false;
+	CurrentTest().TaskSuccess = false;
+}
+
 /*-- Tests --*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 global func Test1_OnStart(int player)
 {
@@ -27,7 +38,7 @@ global func Test1_OnStart(int player)
 
 	CurrentTest().Bot = CreateObject(Clonk, LandscapeWidth() / 2, 260, NO_OWNER);
 	CurrentTest().Item = CurrentTest().Bot->CreateContents(Rock);
-	Wait(30); // wait a little afterwards so that the test does not start right away
+	Wait(10); // wait a little afterwards so that the test does not start right away
 	return true;
 }
 
@@ -58,6 +69,47 @@ global func Test1_Execute()
 
 global func Test1_OnFinished()
 {
-	CurrentTest().Bot->RemoveObject();
-	CurrentTest().Item->RemoveObject();
+	OnTestFinished();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+global func Test2_OnStart(int player)
+{
+	Log("-- Test 2: Run a GetItem-Task, item is in the open --");
+
+	CurrentTest().Bot = CreateObject(Clonk, LandscapeWidth() / 2, 260, NO_OWNER);
+	CurrentTest().Item = CreateObject(Rock, LandscapeWidth() - 20, 190, NO_OWNER);
+	Wait(10); // wait a little afterwards so that the test does not start right away
+	return true;
+}
+
+global func Test2_Execute()
+{
+	if (CurrentTest().AddedAI)
+	{
+		if (CurrentTest().TaskSuccess)
+		{
+			return PassTest();
+		}
+		else
+		{
+			return Wait();
+		}
+	}
+	else
+	{
+		CurrentTest().AddedAI = true;
+	
+		var ai = AI_Local->AddAI(CurrentTest().Bot);
+		ai->SetAgent(AI_Agent_Local);
+		ai.OnTaskSuccess = Global.OnTaskSuccess;
+		Task_GetItem->AddTo(CurrentTest().Bot, 1)->SetItem(Rock);
+		return Wait();
+	}
+}
+
+global func Test2_OnFinished()
+{
+	OnTestFinished();
 }
