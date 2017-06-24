3f703d769a49b3c13b24fa5f62c68ee6a285219d
Mark <markh4b@hotmail.com>
2017-06-24 21:07:44 +0200

    Fix error

--- LibraryAI.ocd/System.ocg/Debug.c
+++ LibraryAI.ocd/System.ocg/Debug.c
@@ -1,3 +1,3 @@
 
-static Debug_TaskCounter = 0; // counter for task IDs
-static Debug_AICounter = 0;   // counter for AI IDs
+static Debug_TaskCounter; // counter for task IDs
+static Debug_AICounter;   // counter for AI IDs
