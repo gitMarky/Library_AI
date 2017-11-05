fdcdce63d139620eb8c528deee1d268dd77a0458
Mark <markh4b@hotmail.com>
2017-11-05 22:16:21 +0100

    Pathfinding: Recalculate path if you are off path

---
+++ LibraryAI.ocd/Extensions.ocd/Agent.ocd/Movement.ocd/Generic.ocd/Script.c
@@ -0,0 +1,21 @@
+/**
+ 
+ */
+
+
+public func Agent_IsBetween(int current_x, int current_y, int start_x, int start_y, int end_x, int end_y)
+{
+	if (_inherited(current_x, current_y, start_x, start_y, end_x, end_y))
+	{
+		return true;
+	}
+	else // is it between start and end?
+	{
+		var angle_to_start = Angle(current_x, current_y, start_x, start_y);
+		var angle_to_end = Angle(current_x, current_y, end_x, end_y);
+		
+		var angle = Abs(Normalize(angle_to_start - angle_to_end, 0));
+		
+		return angle > 90;
+	}
+}
--- LibraryAI.ocd/Extensions.ocd/Agent.ocd/Movement.ocd/Waypoints.ocd/Script.c
+++ LibraryAI.ocd/Extensions.ocd/Agent.ocd/Movement.ocd/Waypoints.ocd/Script.c
@@ -39,8 +39,10 @@ local FxMoveAlongPath = new Effect
 		this.destination = destination;
 		this.x = destination->GetX();
 		this.y = destination->GetY();
+		this.start_x = this.Target->GetX();
+		this.start_y = this.Target->GetY();
 	},
-	
+
 	Timer = func (int time)
 	{
 		// Cancel the whole thing?
@@ -48,41 +50,54 @@ local FxMoveAlongPath = new Effect
 		{
 			return FX_Execute_Kill;
 		}
-	
-		var logic = this.Target->GetAI()->GetAgent();
-		var agent = this.Target;	
 
-		// Something changed? Update the path, but only every 35 frames
-		if (!(time % 35) && (this.x != this.destination->GetX() || this.y != this.destination->GetY()))
+		var waypoint = this.path[this.progress];
+		var logic = this.Target->GetAI()->GetAgent();
+		var agent = this.Target;
+
+		// Something changed? Update the path, but only every 35 frames (displaced by 18, so that it does not recalculate instantly)
+		var interval_ok = (time % 35) == 18;
+		var destination_changed = this.x != this.destination->GetX() || this.y != this.destination->GetY();
+		
+		var dist_path = Distance(this.start_x, this.start_y, waypoint->GetX(), waypoint->GetY());
+		var dist_agent = Distance(this.start_x, this.start_y, agent->GetX(), agent->GetY());
+		var dist_wp = ObjectDistance(agent, waypoint);
+		
+		var off_path = dist_wp >= (6 * dist_path / 5); // logic->Agent_IsBetween(agent->GetX(), agent->GetY(), this.start_x, this.start_y, waypoint->GetX(), waypoint->GetY());
+		if (interval_ok && (destination_changed || off_path))
 		{
+			agent->Message("Recalculating path %d", FrameCounter());
+			// Cancel current command, so that move to works correctly
+			agent->SetCommand("None");
+			// Calculate the path again
 			logic->Agent_MoveTo(agent, this.destination);
 			return FX_OK;
 		}
-	
+
 		// Wait while there is a command, otherwise move
 		if (logic->Agent_IsReadyForCommand(agent))
-		{			
-			// The logic
-			var waypoint = this.path[this.progress];
-
+		{
 			// Reached the waypoint?
 			if (logic->Agent_IsNear(agent, waypoint))
 			{
+				// Update progress and position
 				this.progress += 1;
-				
+				this.start_x = this.Target->GetX();
+				this.start_y = this.Target->GetY();
+
 				// Arrived?
 				if (this.progress >= GetLength(this.path))
 				{
 					if (this.destination)
 					{
-						agent->SetCommand("MoveTo", this.destination);
+						agent->SetCommand("MoveTo", this.destination, nil, nil, nil, nil, 3);
 					}
 					return FX_Execute_Kill;
 				}
 			}
 			else // Move to the waypoint
 			{
-				agent->SetCommand("MoveTo", waypoint);
+				agent->SetCommand("MoveTo", waypoint, nil, nil, nil, nil, 3);
 			}
 		}
 		return FX_OK;
